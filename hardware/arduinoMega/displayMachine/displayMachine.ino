6781119fa960c62a06771c3ea2a249ba0f9c90e8
NelsonWang <wangnelson2@gmail.com>
2021-03-13 18:42:32 -0500

    Display| More setups

--- hardware/arduinoMega/displayMachine/displayMachine.ino
+++ hardware/arduinoMega/displayMachine/displayMachine.ino
@@ -3,8 +3,47 @@
 #include <TouchScreen.h>                  // Touch Support
 
 //CONFIG
-const int TIME_COUNTER_MAX = 25;
+const int COUNTER_MAX = 25;
 
+//DEFINE MODS
+#define MODE_PLAY 0
+#define MODE_PAUSE 1
+
+
+//Tuch screen
+#define TS_MINX 920
+#define TS_MINY 120
+#define TS_MAXX 150
+#define TS_MAXY 940
+#define YP A3                             
+#define XM A2                            
+#define YM 9                             
+#define XP 8                              
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+// macros for color (16 bit)
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#define LCD_CS A3                         
+#define LCD_CD A2                         
+#define LCD_WR A1                         
+#define LCD_RD A0                         
+#define LCD_RESET A4 
+
+                     
+Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+
+//variables
+long counterSegs = 0, timerSegs = 0;
 
 
 void setup() {
