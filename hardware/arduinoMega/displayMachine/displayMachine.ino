694427fe3ed68b1d331e07dc2bc8eb077befc1ba
NelsonWang <wangnelson2@gmail.com>
2021-03-13 18:36:49 -0500

    Display| counter TIMER

--- hardware/arduinoMega/displayMachine/displayMachine.ino
+++ hardware/arduinoMega/displayMachine/displayMachine.ino
@@ -2,6 +2,11 @@
 #include <Elegoo_TFTLCD.h>                // Hardware-specific library
 #include <TouchScreen.h>                  // Touch Support
 
+//CONFIG
+const int TIME_COUNTER_MAX = 25;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
