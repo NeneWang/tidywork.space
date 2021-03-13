aa4ffc2830f1d12015670560010351284aa2decb
NelsonWang <wangnelson2@gmail.com>
2021-03-13 18:33:26 -0500

    aduino mega| setup

--- hardware/NodeMCUE12/GetAndPostRequestSample/GetAndPostRequestSample.ino
+++ hardware/NodeMCUE12/GetAndPostRequestSample/GetAndPostRequestSample.ino
@@ -1,43 +1,65 @@
+#include <ArduinoJson.h>
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
- 
+
 const char* ssid = "FiOS-OMJJ7";
 const char* password = "1010101010";
- 
+
+int workingColumn = 2;
+
 void setup () {
- 
+
   Serial.begin(115200);
- 
+
   WiFi.begin(ssid, password);
- 
+
   while (WiFi.status() != WL_CONNECTED) {
- 
+
     delay(1000);
     Serial.println("Connecting..");
- 
+
   }
- 
+
 }
- 
+
 void loop() {
- 
+
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
- 
+
     HTTPClient http;  //Declare an object of class HTTPClient
- 
-    http.begin("http://wngnelson.com/api/tidywork/api/board.php");  //Specify request destination
+
+    http.begin("http://wngnelson.com/api/tidywork/api/card.php");  //Specify request destination
     int httpCode = http.GET();                                  //Send the request
- 
+
     if (httpCode > 0) { //Check the returning code
- 
+
       String payload = http.getString();   //Get the request response payload
-      Serial.println(payload);             //Print the response payload
- 
+      DynamicJsonDocument doc(8192);
+      deserializeJson(doc, payload);
+      JsonArray arrayOfCards = doc["cards"].as<JsonArray>();
+      for (JsonVariant v : arrayOfCards) {
+        Serial.println(v.as<String>());
+      }
+
+      getFirstCardIndex(doc);
+
+
     }
- 
+
     http.end();   //Close connection
- 
+
   }
- 
-  delay(30000);    //Send a request every 30 seconds
+
+  delay(3000);    //Send a request every 30 seconds
+}
+
+int getFirstCardIndex(DynamicJsonDocument doc) {
+
+  JsonArray arrayOfCards = doc["cards"].as<JsonArray>();
+  for (JsonVariant v : arrayOfCards) {
+    Serial.println(v["cardData"]["columnId"].as<String>());
+  }
+
+  
 }
---
+++ hardware/arduinoMega/displayMachine/displayMachine.ino
@@ -0,0 +1,14 @@
+#include <Elegoo_GFX.h>                   // Core graphics library
+#include <Elegoo_TFTLCD.h>                // Hardware-specific library
+#include <TouchScreen.h>                  // Touch Support
+#include <TimerOne.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
