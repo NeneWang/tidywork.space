16fa0070008afb7ac29da47e5d1ca08af567ec5c
Joseph Nagy <nagyjoseph9@gmail.com>
2021-03-12 20:45:13 -0500

    fixed columns so each one contains unique cards

--- frontend/example_data.js
+++ frontend/example_data.js
@@ -75,6 +75,36 @@ const EXAMPLE_CARD_1 = {
     showModal: false
 }
 
+const EXAMPLE_CARD_2 = {
+    cardId: 2,
+    columnId: 1,
+    name: "Market Research",
+    color: "#912D51",
+    description: `Have to research the market`,
+    deadline: "2021-02-02T23:59",
+    priority: "1",
+    tags: [EXAMPLE_TAG_0, EXAMPLE_TAG_1],
+    comments: EXAMPLE_COMMENT_2,
+    checklists: [],
+    asignedTo: [],
+    showModal: false
+}
+
+const EXAMPLE_CARD_3 = {
+    cardId: 2,
+    columnId: 1,
+    name: "Meeting",
+    color: "#AA2144",
+    description: `Have to attend meeting`,
+    deadline: "2021-02-02T23:59",
+    priority: "1",
+    tags: [EXAMPLE_TAG_0, EXAMPLE_TAG_1],
+    comments: EXAMPLE_COMMENT_2,
+    checklists: [],
+    asignedTo: [],
+    showModal: false
+}
+
 
 // COLUMNS
 const EXAMPLE_COLUMN_0 = {
@@ -94,7 +124,7 @@ const EXAMPLE_COLUMN_1 = {
     name: "Doing",
     color: "#aaaaaa",
     watch: false,
-    cards: [EXAMPLE_CARD_0, EXAMPLE_CARD_1],
+    cards: [EXAMPLE_CARD_2],
     order: "alphabetical",
     showModal: false
 }
@@ -105,7 +135,7 @@ const EXAMPLE_COLUMN_2 = {
     name: "Done",
     color: "#aaaaaa",
     watch: false,
-    cards: [EXAMPLE_CARD_0, EXAMPLE_CARD_1],
+    cards: [EXAMPLE_CARD_3],
     order: "alphabetical",
     showModal: false
 }
